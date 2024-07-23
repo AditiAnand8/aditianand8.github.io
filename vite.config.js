c71c5562c74493f1b95ce401d0bcc9bdf17f9685
Aditi Anand <aditia@umich.edu>
2024-07-23 19:30:34 -0400

    Updating plugin to `    vue({include: [/\.vue$/, /\.md$/],})`

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({include: [/\.vue$/, /\.md$/],}),
     vueDevTools(),
   ],
   resolve: {
