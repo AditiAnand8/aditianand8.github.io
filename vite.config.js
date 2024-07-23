cae7376254cafb19217c96c7a06e0a2e18c19a10
Aditi Anand <aditia@umich.edu>
2024-07-23 19:51:12 -0400

    Adding base.

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue({include: [/\.vue$/, /\.md$/],}),
     vueDevTools(),
   ],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
