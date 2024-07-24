7cdcab643d7930d6b7d0939df14aa688a3bf9a65
Aditi Anand <aditia@umich.edu>
2024-07-23 20:12:20 -0400

    Updating Vue config and `main.js`

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({include: [/\.vue$/, /\.md$/],}),
+    Vue({include: [/\.vue$/, /\.md$/],}),
     vueDevTools(),
   ],
   base: '/',
