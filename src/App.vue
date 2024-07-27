c9486b3839c5517f6fbfc2d865027ccd0cd4b5e3
Aditi Anand <aditia@umich.edu>
2024-07-27 14:43:09 -0400

    Adding tailwind for css

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
